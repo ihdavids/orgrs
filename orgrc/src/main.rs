ef8772ce09f5732140f48f33919e708b7a5d9917
Ian Davids <idavids@roblox.com>
2022-12-07 11:42:59 -0800

    Attempt

--- orgrc/src/main.rs
+++ orgrc/src/main.rs
@@ -1,4 +1,10 @@
 use clap::Parser;
+use jsonrpc_core::futures::future::Future;
+use jsonrpc_core_client::transports::ws;
+use jsonrpc_derive::rpc;
+use serde_json::json;
+use tokio::runtime::Runtime;
+use url::Url;
 
 /// Org Mode Server - provides websocket access to org files.
 #[derive(Parser, Debug)]
@@ -13,8 +19,69 @@ struct Args {
    count: u8,
 }
 
+/// Rpc trait
+#[rpc(client)]
+pub trait Rpc {
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
+
+	/// Adds two numbers and returns a result
+	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+
+	/// Ping server expect return in interval ms
+	#[rpc(name = "ping", raw_params)]
+	fn ping(&self, params: Value) -> Result<String>;
+
+	/// Performs asynchronous operation
+	#[rpc(name = "callAsync")]
+	fn call(&self, a: u64) -> FutureResult<String, Error>;
+}
+
 
 fn main() {
     let args = Args::parse();
     println!("Hello, world! {}", args.name);
+
+    let mut rt = Runtime::new().unwrap();
+
+	let client_url = Url::parse("ws://127.0.0.1:8888/kurento").unwrap();
+	let client = rt.block_on(ws::connect::<gen_client::Client>(&client_url)).unwrap();
+
+	let mut interval = serde_json::map::Map::new();
+	map.insert("interval".to_string(), 1000.into());
+
+	client
+           .clone()
+           .ping(json!({"interval": 1000}).into())
+           .map(|res| println!("ping = {}", res))
+           .wait()
+	   .unwrap();
+
+	rt.shutdown_now().wait().unwrap();
+
+
+}
+
+
+
+/// Rpc trait
+#[rpc(client)]
+pub trait Rpc {
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
+
+	/// Adds two numbers and returns a result
+	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+
+	/// Ping server expect return in interval ms
+	#[rpc(name = "ping", raw_params)]
+	fn ping(&self, params: Value) -> Result<String>;
+
+	/// Performs asynchronous operation
+	#[rpc(name = "callAsync")]
+	fn call(&self, a: u64) -> FutureResult<String, Error>;
 }
--- orgrs/src/server.rs
+++ orgrs/src/server.rs
@@ -5,6 +5,58 @@ use jsonrpc_ws_server::jsonrpc_core::*;
 //use ws;
 //use self::server_utils::tokio;
 
+//use jsonrpc_core_client::transports::local;
+use jsonrpc_core::futures::future::{self};
+//use jsonrpc_core::futures::{self, Future};
+//use jsonrpc_core::{self, FutureResult};
+use jsonrpc_core::{BoxFuture, IoHandler, Result};
+use jsonrpc_derive::rpc;
+//use futures_util::future::*;
+
+/// Rpc trait
+#[rpc]
+pub trait Rpc {
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
+
+	/// Adds two numbers and returns a result
+	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+
+	/// Performs asynchronous operation
+	#[rpc(name = "callAsync")]
+	fn call(&self, a: u64) -> BoxFuture<Result<String>>;
+}
+
+struct RpcImpl;
+
+impl Rpc for RpcImpl {
+	fn protocol_version(&self) -> Result<String> {
+		Ok("version1".into())
+	}
+
+	fn add(&self, a: u64, b: u64) -> Result<u64> {
+		Ok(a + b)
+	}
+
+	fn call(&self, _: u64) -> BoxFuture<Result<String>> {
+        Box::pin(future::ready(Ok("OK".to_owned())))
+		//future::ok("OK".to_owned())
+	}
+}
+/* 
+fn main() {
+	let mut io = IoHandler::new();
+	io.extend_with(RpcImpl.to_delegate());
+
+	let fut = {
+		let (client, server) = local::connect::<gen_client::Client, _, _>(io);
+		client.add(5, 6).map(|res| println!("5 + 6 = {}", res)).join(server)
+	};
+	fut.wait().unwrap();
+}
+*/
 
 pub struct OrgServer
 {
