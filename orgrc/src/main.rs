bbdb113dde022078a8f1df2b087a1c407f7bc6e3
Ian Davids <ihdavids@gmail.com>
2022-12-07 00:12:55 -0800

    Working on arguments and servers

--- orgrc/src/main.rs
+++ orgrc/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Org Mode Server - provides websocket access to org files.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Name of the person to greet
+   #[arg(short, long)]
+   name: String,
+
+   /// Number of times to greet
+   #[arg(short, long, default_value_t = 1)]
+   count: u8,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, world! {}", args.name);
 }
