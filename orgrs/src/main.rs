4b8c3d4caff41fbd6931dccd5b9dd6d14b57c297
Ian Davids <ihdavids@gmail.com>
2022-12-07 00:01:49 -0800

    Cluster rust

--- orgrs/src/main.rs
+++ orgrs/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+mod server;
+
+/// Org Mode Server - provides websocket access to org files.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Name of the person to greet
+   #[arg(short, long)]
+   name: String,
+
+   /// Number of times to greet
+   #[arg(short, long, default_value_t = 1)]
+   count: u8,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let server = server::OrgServer {};
+    server.Start();
+    println!("Hello, world! {}", args.count);
 }
---
+++ orgrs/src/server.rs
@@ -0,0 +1,45 @@
+use jsonrpc_ws_server::*;
+use jsonrpc_ws_server::jsonrpc_core::*;
+extern crate jsonrpc_core;
+use jsonrpc_core::*;
+use jsonrpc_core::futures::Future;
+
+//use jsonrpc_core;
+//use ws;
+//use self::server_utils::tokio;
+
+
+pub struct OrgServer
+{
+
+}
+
+impl OrgServer 
+{
+    pub fn Start(&self)
+    {
+        /* 
+        let mut io = IoHandler::default();
+        io.add_method("say_hello", |_params: Params| async {
+            Ok(Value::String("hello".to_owned()))
+        });
+    
+        let server = ServerBuilder::new(io)
+            .threads(3)
+            .start_http(&"127.0.0.1:3030".parse().unwrap())
+            .unwrap();
+    
+        server.wait();
+*/
+        let mut io = IoHandler::new();
+        io.add_method("say_hello", |_| async {
+            Ok(Value::String("Hello World!".into()))
+        });
+    
+        let server = ServerBuilder::new(io)
+            .start(&"0.0.0.0:3030".parse().unwrap())
+            .expect("Server must start with no issues");
+    
+        server.wait().unwrap()    
+    }
+}
