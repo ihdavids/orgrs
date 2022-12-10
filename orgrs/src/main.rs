4527ee983470a8f7cd81e0e971af0614b191f43d
Ian Davids <idavids@roblox.com>
2022-12-09 22:10:14 -0800

    Something is loading.

--- orgrs/src/main.rs
+++ orgrs/src/main.rs
@@ -2,17 +2,23 @@ use clap_conf::*;
 mod server;
 mod orgdb;
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
 	let args = clap_app!(orgrc => 
 								(version: crate_version!())
 								(author: "Ian Davids")
 								(about: "OrgRs Org Mode Server")
-								(@arg connect: -c "Server connection")
+								(@arg connect: -c --connect +takes_value "Server connection")
+								(@arg org: -o --orgglob +takes_value "org glob")
 							).get_matches();
 	let cfg = clap_conf::with_toml_env(&args, &["{HOME}/.config/orgrs/init.toml","{HOME}/.orgrs.toml","./.orgrs.toml"]);
 	let connect_str = cfg.grab().arg("connect").conf("server.connect").env("ORGRS_CONNECT").def("ws://127.0.0.1:3030/orgrs");
+	let org_glob = cfg.grab().arg("org").conf("org.glob").env("ORGRS_GLOB").def("**/*.org");
 
-    let db = orgdb::OrgDb {};
+    let mut db = orgdb::OrgDb::new();
+    db.reload_all(&org_glob).await;
+    db.list_all_files().await;
 
     let server = server::OrgServer {};
     server.start(&connect_str);
--- orgrs/src/orgdb.rs
+++ orgrs/src/orgdb.rs
@@ -1,5 +1,7 @@
 use orgize::{Org, ParseConfig};
 use tokio::io::AsyncReadExt;
+use glob::glob;
+use std::collections::HashMap;
 
 /* 
 Org::parse_custom(
@@ -12,12 +14,36 @@ Org::parse_custom(
 );
 */
 
-pub struct OrgDb;
+pub struct OrgDb<'a> {
+    pub byFile: HashMap<String,Org<'a>>
+}
 
-impl OrgDb {
-   async fn parse_org_file(filename: &String) -> Result<Org,std::io::Error>
+impl OrgDb<'_> {
+   pub fn new<'a>() -> OrgDb<'a> {
+        OrgDb { byFile: HashMap::new() }
+   }
+
+   pub async fn parse_org_file<'a>(filename: &String) -> Result<Org<'a>,std::io::Error>
    {
+        println!("READING: {}", filename.as_str());
         let contents = tokio::fs::read_to_string(filename).await?;
+        println!("PARSING: {}", filename.as_str());
         Ok(Org::parse_string(contents))
    }
+
+   pub async fn reload_all(&mut self, path: &String) {
+        for entry in glob(path.as_str()).unwrap() {
+            let f = entry.unwrap();
+            let name = f.as_os_str().to_str().unwrap();
+            let nm = String::from(name);
+            let org: Org = OrgDb::parse_org_file(&nm).await.unwrap();
+            self.byFile.insert(nm.clone(), org);
+        }
+    }
+
+    pub async fn list_all_files(&self) {
+        for (name, org) in &self.byFile {
+            println!("-> {name}");
+        }
+    }
 }
