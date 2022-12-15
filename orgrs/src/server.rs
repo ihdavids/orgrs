3f707695c975fd0ac25d6dc114e5b61652d2fe38
Ian Davids <idavids@roblox.com>
2022-12-14 17:09:09 -0800

    Attempting to add expression parsing test.

--- orgrs/src/server.rs
+++ orgrs/src/server.rs
@@ -4,6 +4,8 @@ use jsonrpc_core::{BoxFuture, IoHandler, Result};
 use std::net::SocketAddr;
 use orgcom::Rpc;
 use log::{info};
+use crate::orgdb::OrgDb;
+use fasteval;
 
 struct RpcImpl;
 
@@ -23,6 +25,50 @@ impl Rpc for RpcImpl {
 
 	fn query_headline(&self, query: String) -> Result<Vec<String>> {
 		let r:Vec<String> = Vec::new();
+		let hold = OrgDb::get();
+        let ydb = hold.lock().expect("Failed to access OrgDb in Query Headline");
+
+		/* 
+	    // This example doesn't use any variables, so just use an EmptyNamespace:
+ 	    let mut ns = fasteval::EmptyNamespace;
+    	let parser = fasteval::Parser::new();
+    	let mut slab = fasteval::Slab::new();
+    	//let mut map = BTreeMap::new();
+	    let mut cb = |name:&str, args:Vec<f64>| -> Option<f64> {
+ 	    	let mydata : [f64; 3] = [11.1, 22.2, 33.3];
+  	    	match name {
+            // Custom constants/variables:
+            "x" => Some(3.0),
+            "y" => Some(4.0),
+
+            // Custom function:
+            "sum" => Some(args.into_iter().sum()),
+
+            // Custom array-like objects:
+            // The `args.get...` code is the same as:
+            //     mydata[args[0] as usize]
+            // ...but it won't panic if either index is out-of-bounds.
+            "data" => args.get(0).and_then(|f| mydata.get(*f as usize).copied()),
+
+            // A wildcard to handle all undefined names:
+            _ => None,
+     	   }
+    	};
+
+
+    	let expr_str = "sin(deg/360 * 2*pi())";
+    	let compiled = parser.parse(expr_str, &mut slab.ps)?.from(&slab.ps).compile(&slab.ps, &mut slab.cs);
+    	for deg in 0..360 {
+        	//map.insert("deg".to_string(), deg as f64);
+        	// When working with compiled constant expressions, you can use the
+        	// eval_compiled*!() macros to save a function call:
+        	let val = fasteval::eval_compiled!(compiled, &slab, &mut cb);
+        	//eprintln!("sin({}°) = {}", deg, val);
+    	}
+*/
+        for (name, _) in &ydb.by_file {
+            println!("-> {name}");
+        }
 		return Ok(r)
 	}
 }
