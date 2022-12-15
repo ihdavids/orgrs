04a2b934235642773b238a7882304adaf2dff824
Ian Davids <idavids@roblox.com>
2022-12-14 16:09:54 -0800

    Org db is kind of working

--- orgrs/src/orgdb.rs
+++ orgrs/src/orgdb.rs
@@ -1,7 +1,7 @@
 use orgize::{Org, ParseConfig};
 use tokio::io::AsyncReadExt;
 use glob::glob;
-use std::{collections::HashMap, path::Path, borrow::BorrowMut};
+use std::{collections::HashMap, path::Path, borrow::BorrowMut, io::Read};
 use notify::{EventKind, Watcher, RecommendedWatcher, RecursiveMode, ReadDirectoryChangesWatcher};
 use std::sync::{Arc, Weak, Mutex};
 use once_cell::sync::Lazy;
@@ -52,6 +52,25 @@ impl OrgDb<'_> {
         Ok(Org::parse_string(contents.to_string()))
    }
 
+   pub fn parse_org_file_sync<'a>(filename: &String) -> Result<Org<'a>,std::io::Error>
+   {
+        println!("READING SYNC: {}", filename.as_str());
+        // Have UTF-8 problems with this pathway.
+        //let contents = tokio::fs::read_to_string(filename).await?;
+
+        // This is more expensive than the alternative but it can handle non UTF-8 Files
+        // Not sure which one I should support. Or optional?
+        let mut buf = vec![];
+        {
+            let mut file = std::fs::File::open(filename)?;
+            file.read_to_end(&mut buf)?;
+        }
+        let contents = String::from_utf8_lossy (&buf);
+
+        println!("PARSING SYNC: {}", filename.as_str());
+        Ok(Org::parse_string(contents.to_string()))
+   }
+
    pub async fn reload_all(&mut self, path: &String) {
         for entry in glob(path.as_str()).unwrap() {
             let f = entry.unwrap();
@@ -62,9 +81,15 @@ impl OrgDb<'_> {
         }
     }
 
-    pub async fn reload(&mut self, nm: &String) {
-        let org: Org = OrgDb::parse_org_file(nm).await.unwrap();
+    pub fn reload(&mut self, nm: &String) {
+        let org: Org = OrgDb::parse_org_file_sync(nm).unwrap();
+        println!("RELOADING: {}",nm);
         self.by_file.insert(nm.clone(), org);
+    }
+
+    pub fn delete(&mut self, nm: &String) {
+        println!("DELETING: {}",nm);
+        self.by_file.remove(nm);
     }
 
     pub async fn list_all_files(&self) {
@@ -77,20 +102,32 @@ impl OrgDb<'_> {
             match res {
                 Ok(event) => {
                     println!("event: {:?}", event);
-                    match event.kind {
-                        Modify => {
-                            let mut db = OrgDb::get();
-                            for name in event.paths {
-                                let tmp = String::from(name.to_string_lossy());
-                                let mut xdb = db.borrow_mut();
-                                        match xdb.lock() {
-                                            Ok(mut ydb) => {
-                                                ydb.reload(&tmp);
-                                            }
-                                            Err(e) => {
-                                                println!("Failed to update, could not aquire lock!");
-                                            }
-                                        }
+                    if event.kind.is_modify() || event.kind.is_create() {
+                        let mut db = OrgDb::get();
+                        for name in event.paths {
+                            let tmp = String::from(name.to_string_lossy());
+                            let xdb = db.borrow_mut();
+                            match xdb.lock() {
+                                Ok(mut ydb) => {
+                                    ydb.reload(&tmp);
+                                }
+                                Err(e) => {
+                                    println!("Failed to update, could not aquire lock!");
+                                }
+                            }
+                        }
+                    } else if event.kind.is_remove() {
+                        let mut db = OrgDb::get();
+                        for name in event.paths {
+                            let tmp = String::from(name.to_string_lossy());
+                            let xdb = db.borrow_mut();
+                            match xdb.lock() {
+                                Ok(mut ydb) => {
+                                    ydb.delete(&tmp);
+                                }
+                                Err(e) => {
+                                    println!("Failed to update, could not aquire lock!");
+                                }
                             }
                         }
                     }
@@ -103,7 +140,7 @@ impl OrgDb<'_> {
 
     pub fn watch(path: &String) -> notify::Result<()> {
         let mut db = OrgDb::get();
-        let mut xdb = db.borrow_mut();
+        let xdb = db.borrow_mut();
         match xdb.lock() {
             Ok(mut ydb) => {
                 ydb.watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
